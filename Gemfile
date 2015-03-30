bc6faad30a575ce487144fd97d25c4881337cff8
Sam Davies <sam@visualputty.co.uk>
2015-03-30 09:16:46 -0500

    Create the beginnings of a scrip that'll create what we're after
    This should create a project with the correct build target, and a
    workspace that contains it. Need to be able to create a playground

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'http://rubygems.org'
 
 
 gem "xcodeproj"
+
+group :development do
+  gem 'pry'
+end
---
+++ swingset.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+require 'bundler/setup'
+
+# require your gems as usual
+require 'xcodeproj'
+
+# Create a new project
+proj = Xcodeproj::Project.new("~/Desktop/FrameworkProvider.xcodeproj")
+
+# Add a new aggregate target
+# Target
+target = proj.new(Xcodeproj::Project::Object::PBXAggregateTarget)
+proj.targets << target
+target.name = "FrameworkProvider"
+target.product_name = "FrameworkProvider"
+target.build_configuration_list = Xcodeproj::Project::ProjectHelper.configuration_list(proj, :osx, nil, nil, :swift)
+
+# Create a "copy files" build phase, and add it
+copy_files = target.new_copy_files_build_phase()
+
+# Configure the output location of build files
+copy_files.symbol_dst_subfolder_spec = :products_directory
+
+target.pretty_print
+
+# Add the relevant files to the project and to the copy files build phase
+
+f1 = proj.new_file("/Users/sam/dev/temp/dl/Carthage/Build/Mac/Argo.framework")
+f2 = proj.new_file("/Users/sam/dev/temp/dl/Carthage/Build/Mac/Runes.framework")
+
+copy_files.add_file_reference(f1)
+copy_files.add_file_reference(f2)
+
+# Save the project
+proj.save
+
+# Create a new workspace with new project
+ws = Xcodeproj::Workspace.new([])
+
+ws << proj.path
+
+# Save the workspace
+ws.save_as("/Users/sam/Desktop/PlaygroundWithFrameworks.xcworkspace")
+
+# Add a playground to the new work space
+
+
+# Save it all
+
+
+# Open the workspace
+
